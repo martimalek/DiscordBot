5d3915f4eb7e715b64f8a8099a8864f0f297a7e8
martimalek <martimalekdot@hotmail.es>
2020-12-15 20:20:08 +0000

    Added a basic discord example bot

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-# Hi!
+import discord
+import os
+
+client = discord.Client()
+
+forbidden_words = ['subnormal', 'mongolo', 'retrasao']
+
+@client.event
+async def on_ready():
+  print('I\'ve been born as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  
+  msg = message.content
+
+  if msg.startswith('$bot'):
+    await message.channel.send('F*ck you!')
+
+  # any(word in msg for word in forbidden_words)
+  if any(word in msg for word in forbidden_words):
+    await message.channel.send('A mi no me insultes eh')
+    # await message.channel.send('{0} tu madre!'.format(word))
+
+client.run(os.getenv('DISCORD_TOKEN'))
